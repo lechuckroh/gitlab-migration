f505548ffa7584e78ef2eab9913221d3ed57bb12
Lechuck Roh <member@lechuck2011.cafe24.com>
2020-08-03 16:55:11 +0900

    fix #9 private class methods naming convention

--- migrate.py
+++ migrate.py
@@ -14,18 +14,18 @@ class GitLabMigration:
         self.gl_dest = gl_dest
 
     def migrate(self, src_group_id, dest_group_id):
-        self.__migrate_groups(src_group_id, dest_group_id)
+        self._migrate_groups(src_group_id, dest_group_id)
         print("migration finished.")
 
-    def __migrate_groups(self, src_group_id, dest_group_id):
+    def _migrate_groups(self, src_group_id, dest_group_id):
         """
         Migrate a group
         """
         src_group = self.gl_src.groups.get(src_group_id)
-        src_dict = self.__get_subgroups_by_name(src_group)
+        src_dict = self._get_subgroups_by_name(src_group)
 
         dest_group = self.gl_dest.groups.get(dest_group_id)
-        dest_dict = self.__get_subgroups_by_name(dest_group)
+        dest_dict = self._get_subgroups_by_name(dest_group)
 
         # migrate subgroups
         for subgroup_name, src_subgroup in src_dict.items():
@@ -33,35 +33,35 @@ class GitLabMigration:
             dest_subgroup = dest_dict.get(subgroup_name)
             if dest_subgroup is None:
                 print(f"[+ GROUP] {dest_group.name} / {subgroup_name}")
-                dest_subgroup = self.__create_subgroup(dest_group, subgroup_name)
+                dest_subgroup = self._create_subgroup(dest_group, subgroup_name)
 
-            self.__migrate_groups(src_subgroup.id, dest_subgroup.id)
+            self._migrate_groups(src_subgroup.id, dest_subgroup.id)
 
         # migrate projects
-        src_project_dict = self.__get_projects_by_name(src_group)
-        dest_project_dict = self.__get_projects_by_name(dest_group)
+        src_project_dict = self._get_projects_by_name(src_group)
+        dest_project_dict = self._get_projects_by_name(dest_group)
         for project_name, src_project in src_project_dict.items():
             # create dest project if not exists
             dest_project = dest_project_dict.get(project_name)
             if dest_project is None:
                 print(f"[+ PROJECT] {dest_group.path} / {project_name}")
-                dest_project = self.__create_project(dest_group, project_name)
+                dest_project = self._create_project(dest_group, project_name)
 
-            self.__migrate_project(src_project, dest_project)
+            self._migrate_project(src_project, dest_project)
 
-    def __create_subgroup(self, group, name):
+    def _create_subgroup(self, group, name):
         """
         Create subgroup under 'group_id' group (destination gitlab)
         """
         return self.gl_dest.groups.create({'name': name, 'path': name, 'parent_id': group.id})
 
-    def __create_project(self, group, name):
+    def _create_project(self, group, name):
         """
         Create project under 'group_id' group (destination gitlab)
         """
         return self.gl_dest.projects.create({'name': name, 'namespace_id': group.id})
 
-    def __migrate_project(self, src_project, dest_project):
+    def _migrate_project(self, src_project, dest_project):
         """
         Migrate a project
         """
@@ -93,14 +93,14 @@ class GitLabMigration:
         print(f"[done] {src_project.path_with_namespace}")
 
     @staticmethod
-    def __get_subgroups_by_name(group):
+    def _get_subgroups_by_name(group):
         result = dict()
         for subgroup in group.subgroups.list(per_page=100):
             result[subgroup.name] = subgroup
         return result
 
     @staticmethod
-    def __get_projects_by_name(group):
+    def _get_projects_by_name(group):
         result = dict()
         for project in group.projects.list(per_page=100):
             result[project.name] = project
